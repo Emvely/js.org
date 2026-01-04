3a2858973b35167a6663e71f0ce9a24c0a422379
Emvely <54516378+Emvely@users.noreply.github.com>
2026-01-05 01:07:31 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3700,6 +3700,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "redbook": "su.bioke.com"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
